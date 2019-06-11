1e911f55e200cea58f5ab943864a6e66215b2bc2
Vikas Chaudhary <M1052611@mindtree.com>
2019-06-11 15:59:45 -0400

    Adding Eureka dependencies and properties

--- src/main/java/com/mindtree/poc/demoSpringCloudConfig/DemoSpringCloudConfigApplication.java
+++ src/main/java/com/mindtree/poc/demoSpringCloudConfig/DemoSpringCloudConfigApplication.java
@@ -2,10 +2,14 @@ package com.mindtree.poc.demoSpringCloudConfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+// This for enabling the config server
 @EnableConfigServer
+//This is for registering the config server with discovery server
+@EnableDiscoveryClient
 public class DemoSpringCloudConfigApplication {
 
 	public static void main(String[] args) {
