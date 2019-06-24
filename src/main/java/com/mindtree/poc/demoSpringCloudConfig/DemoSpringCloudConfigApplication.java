f2d1fef5a557ff4e0d17a53e80a0baedc6ef448b
vikas2453 <vikas2453@gmail.com>
2019-06-24 15:45:24 -0400

    removing exclude

--- src/main/java/com/mindtree/poc/demoSpringCloudConfig/DemoSpringCloudConfigApplication.java
+++ src/main/java/com/mindtree/poc/demoSpringCloudConfig/DemoSpringCloudConfigApplication.java
@@ -1,15 +1,18 @@
 package com.mindtree.poc.demoSpringCloudConfig;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
 // This for enabling the config server
 @EnableConfigServer
 //This is for registering the config server with discovery server
 @EnableDiscoveryClient
+
 public class DemoSpringCloudConfigApplication {
 
 	public static void main(String[] args) {
