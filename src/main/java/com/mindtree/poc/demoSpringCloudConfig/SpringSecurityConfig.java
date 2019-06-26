84e8e8f1fc9b54a68a6af33f94a2159a062a872a
vikas2453 <vikas2453@gmail.com>
2019-06-26 11:16:27 -0400

    Removed some of the properties from config server property file

---
+++ src/main/java/com/mindtree/poc/demoSpringCloudConfig/SpringSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.mindtree.poc.demoSpringCloudConfig;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+//@EnableWebSecurity
+public class SpringSecurityConfig{
+/*public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	 @Override
+	    protected void configure(HttpSecurity http) throws Exception{
+	        http.authorizeRequests().antMatchers("/").permitAll();
+	    }*/
+}
